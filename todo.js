8c3bf949dc8ec444dccbf7c57bbedbdf68375e72
ohyoungil <oyi00111@gmail.com>
2020-04-01 18:21:42 +0900

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,7 @@
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = form.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+function init() {}
+
+init();
